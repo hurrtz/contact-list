805c3de3015a0ab04b2d61dd61bce7f68a39d5c2
Tobias Winkler <info@tobiaswinkler.berlin>
2019-11-15 18:08:05 +0100

    fixed import
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Router, Switch, Route, Link, Redirect } from './utils/router'; // we prepared react-router for you here
-import { isDesktop, isIos } from './utils/router'; // we prepared some useful booleans for you here
+import { isDesktop, isIos } from './utils/common'; // we prepared some useful booleans for you here
 import Contacts from './components/Contacts';
 
 class App extends Component {
